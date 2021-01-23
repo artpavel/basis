a1d68402eafcb3b42e6cf1d1d2bdef03ffed4aa6
artpavel <artpavel@ukr.net>
2021-01-23 16:16:31 +0200

    add mod

---
+++ Лекция_52_пространство_имен/module4/SpeakGoodBye.js
@@ -0,0 +1,8 @@
+(function(window) {
+    let byeSpeaker = {};
+    let speakWord = "Good Bye";
+    byeSpeaker.speak = function(name) {
+        console.log(speakWord + " " + name);
+    }
+    window.byeSpeaker = byeSpeaker;
+})(window);
